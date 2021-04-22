9aee9cfb7891073958da89f4406d93be488bafe6
KienNT <kiennt@kaopiz.com>
2021-04-22 14:24:43 +0700

    Update code base

--- lib/flutter_package_base.dart
+++ lib/flutter_package_base.dart
@@ -1,7 +1,3 @@
 library flutter_package_base;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/widget/ink_well_widget.dart';
